564ad48aee01022b1b63d0fb3918c83db9f32c6d
Francis Lukoki <francislukoki@hotmail.fr>
2023-02-19 18:42:09 +0100

    fin premiere page

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { data } from '../data'
+import { test } from '../test'
 
 const Book = () => {
+
     return (
         <>
-            <h2 className='flex justify-center text-3xl my-5'>Mes ouvrages</h2>
-            <div>
-
+            <h2 className='flex justify-center text-3xl my-5 '>Mes ouvrages</h2>
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-4 mx-6'>
+                {data.map((data) => (
+                    <div key={data.id} className="max-w-sm rounded overflow-hidden shadow-lg ">
+                        <img className='h-60 mx-auto' src={data.image} alt="img-text" />
+                        <div className='font-bold mt-4 px-3'>
+                            {data.title}
+                        </div>
+                    </div>
+                ))}
             </div>
         </>
     )
--- src/data.js
+++ src/data.js
@@ -1,20 +1,104 @@
 export const data = [
+
+
     {
         id: 1,
-        title: "Ebembe ya Thomson",
-        description: {
+        title: "Collage caricatural sur la Tshibilie",
+        description: "CONGOLISATION ? Voici ci-après, un extrait de ce que Mobutu Sese Seko avait dit à propos de la congolisation, le 04 octobre 1973 du haut de la Tribune des Nations Unies : « Certains d’entre vous consultent peut-être le dictionnaire pour connaître la définition du mot anarchie, alors qu’au Zaïre nous l’avons tellement vécue que nombre de personnes croyaient que le mot anarchie était une invention zaïroise. C’est pourquoi tout désordre, dans n’importe quelle partie du monde était baptisé du nom de CONGOLISATION». Curieusement, même à l’heure actuelle nous assistons constamment à la montée de la congolisation dont Mobutu faisait allusion à son époque. Et pour démontrer la recrudescence de cette congolisation, l’auteur s’était engagé à écrire cet ouvrage sur le Tohu-bohu qui étouffe la RDC depuis l’évènement de LAMUKA, géré par la Fondation Koffi Annan à Genève, en novembre 2018. « Mais quel genre de livre présenter au public pour éviter de tomber dans un académisme austère à l’égard de la population perpétuellement meurtrie ? », se posait-il la question. Celle-ci l’a préoccupé pendant des jours voire des mois qu’aussitôt pensé, il s’était lancé à faire des recherches, à lire des livres et journaux et à suivre des nouvelles radiotélévisées et à naviguer sur l’internet. C’est alors que l’art sarcastique du collage caricatural utilisé par le You-tubeur Claude Bongambo alias Balia Ngando – à ne pas confondre avec l’image manipulée pour des raisons d’intox –, lui a paru très attrayant pour mettre en exergue la TSHIBILIE, un acronyme formé sur base de tshi (de Tshilombo) et de Bilie (bila) provenant de Kabila. La TSHIBILIE est une kleptocratie avérée. Elle est à la base de la Renaissance de l’Anarchie dite CONGOLISATION, un véhicule, capable de conduire la RDC à l’effondrement. Vu sous cet angle, l’ouvrage caricatural que voici correspond à l'une de ces petites pierres qui s'insère dans l’édification d’un grand mur capable d’empêcher la dislocation de la RDC. Ainsi donc, cet ouvrage s'inscrit dans l'objectif de libérer la RDC par la voie des Révoltes logiques des populations, disons le soulèvement populaire dans toutes ses diverses astuces.",
+        image: "https://faltercdn.s3.eu-central-1.amazonaws.com/img/978395/038/9783950384093_L.jpg",
+        price: 42.60,
+        lang: {
             francais: "Français",
-            allemand: "Lingala",
-            anglais: "Anglais",
-            lingala: "Lingala",
-        },
-        image: "https://m.media-amazon.com/images/I/41Qp8VHvUFL.jpg",
+        }
+    },
+    {
+        id: 2,
+        title: "Qu'est-ce qui nous manque ?",
+        description: "Mobutu Sese Seko, qui avait coutume de dire «Avant moi le chaos, après moi le déluge», est mort malade en exil à Rabat le 7 septembre 1997. Laurent Désiré Kabila est mort assassiné dans son bureau de Kinshasa, le 16 janvier 2001. Le premier a prophétisé le «déluge» qui pourrait à la fois symboliser la destruction et l‘instauration d‘un nouvel ordre, donc la purification. Le deuxième, qui a fait son entrée triomphale en mai 1997 à Kinshasa, accompagné d’une sangsue toxique collée sur sa poitrine gauche, a prescrit à la République Démocratique du Congo une cure de «Congomicine» qui s’est avérée potentiellement destructrice. L’auteur de cette esquisse, surtout destinée au public rd-congolais, a tenté de mettre au pilori la majorité de la classe politique rd-congolaise qui n’ose pas dénoncer en toute franchise l’occupation de la République Démocratique du Congo gérée par la sangsue «kabilienne» hissée sur le trône de la RDC pour mieux servir les maîtres du monde au détriment de la population congolaise.",
+        image: "https://m.media-amazon.com/images/I/51kXtyJk9GL.jpg",
+        price: 18.80,
+        lang: {
+            francais: "Français",
+        }
+    },
+    {
+        id: 3,
+        title: "Congo 2000 : Fin du temps ou nouvelle naissance ?",
+        description: `La version originale de ce livre a été publiée en allemand en janvier 1999 dans les Editions Ploechl en Autriche sous le titre « Kongo – Endzeit oder Wende ? » Et quelques jours après sa publication – le 29 janvier 1999 – le prestigieux quotidien autrichien « Der Standard » l’a intitulé comme livre du jour. Et en Avril 2000, ce livre a été traduit et actualisé en français pour des lecteurs francophones et surtout congolais. Sa présentation à l’université de Québec à Montréal en mars 2001, deux mois après l’assassinat de Laurent Désiré Kabila, a été accompagnée d’un débat houleux.
+            Quinze ans après sa publication, cet ouvrage n’a rien perdu d’actualité et il reste ainsi une source d’arguments pouvant apporter aux lecteurs curieux des éclaircissements sur des rébellions récurrentes à l’est de la RDC. Le contenu reste le même. Sauf que des noms et mots orthographiquement non correctes suite à des difficultés connues lors de la conversion de l’allemand en français sont ici corrigés.
+            Il faut donc se mettre dans le bain du temps de Laurent-Désiré Kabila en tant que président de la RDC en lisant cette version d’e-book : Paul Kagame n’était pas encore président de Rwanda et James Kabarere, l’actuel ministre de défense de Rwanda, était chef d’état-major des armées de la RDC.
+            La République Démocratique du Congo avait 40 ans lorsque la version française de ce livre a été publiée. Le contexte du temps : Quarante années après son indépendance, la République démocratique du Congo est au bord de l’abîme. Rien ne marche : économiquement, culturellement et moralement. Ici des sectes prophétisent aveuglement un meilleur temps proche ; et là des milliers de masqués vivant dans un monde d’apparence et incapables d’ôter seuls leurs masques, afin de se mettre à bâtir un beau pays dans la paix comme il a été chanté sous le doux soleil du 30 juin 1960. Néanmoins, un sincère dialogue intercongolais s’avère nécessaire afin d’implanter la paix dans la cuvette centrale. Sans une paix issue d’un tel dialogue, l’unité congolaise est illusoire. Illusoire serait aussi la construction de ce beau pays tant chanté.
+            Renaissance ou nouvelle naissance ? Espérance-François Ngayibata BULAYUMI propose ici un plaidoyer pour une nouvelle naissance congolaise qui n’est qu’un discours contre le démembrement de la RDC.
+            Quiconque lit cet ouvrage de bonne foi et à peu près se rendra vite compte que l’auteur souffre de maux provoqués par la tragédie congolaise et qu’il ne s’imagine pas que l’auteur s’est épargné dans sa critique faite aux congolais. En fait, sa déculpabilisation au sort des congolais n’est en aucun cas l’ambition de ce livre qui, de prime abord, se définit comme lamentation, car fin du temps ou nouvelle naissance en RDC le concerne aussi, tout comme bien de nombreux congolais.`,
+        image: "https://m.media-amazon.com/images/I/51nT0ejf7mL.jpg",
         price: 5,
         lang: {
             francais: "Français",
-            allemand: "Lingala",
-            anglais: "Anglais",
+        }
+    },
+    {
+        id: 4,
+        title: "Mosuni",
+        description: "Mosuni (Fleisch) ist sowohl eine affirmative Antwort auf das Jahr der afrikanischen Sprachen 2006, eine Initiative der Afrikanischen Union (AU), als auch eine Hommage an eine der bedeutendsten Sprachen der zeitgenössischen afrikanischen Lieder: Lingala. Das Buch spiegelt die Seele des Kongobeckens in allen Tönen, Farben und Reichtümern, die Geschichte, Mythen und Religion wider. Es moralisiert und politisiert nicht und doch ist diese Erzählung neben allem Zauber politisch und religiös. Mosuni ist auch als Metapher zu betrachten, die Identitätsfindung und eine mögliche friedliche Koexistenz zwischen verschiedenen Volksgruppen und Weltanschauungen im Kongobecken thematisiert. Mosuni ist eine Literaturdarstellung in Form einer Fiktion, die sich teils auf die Realität stützt. Wer die Herkunft und Zukunft des Kongobeckens verstehen will, für den ist dieses Buch eine wichtige literarische Basis.",
+        image: "https://m.media-amazon.com/images/I/81L7Dv90RbL.jpg",
+        price: 16.99,
+        lang: {
             lingala: "Lingala",
+        }
+    },
+    {
+        id: 5,
+        title: "Ebembe ya Thomson",
+        description: `« Ebembe ya Thomson », roman écrit entièrement en lingala
+            Dépendance et aliénation sont sœurs. L’aliénation alimentaire a ouvert la porte à la dépendance alimentaire voire même culturelle, dont des générations de congolaises et congolais sont aujourd’hui victimes. Cette dépendance alimentaire n’est autre qu’un des fruits des vents semés en RDC. Le roman « Ebembe ya Thomson » écrit entièrement en Lingala décrit, sous l’angle de la critique sociale, l’apogée du poisson chinchard alias Thomson, dernière qualité de capture de la pêche maritime industrielle sur les marchés de Kinshasa. Et pour cause, le contenu du livre cloue au pilori la prédominance mercantile de ce poisson omniprésent dans l’assiette des kinois en dénonçant d’une façon humoristique des trafics nuisibles de certains produits alimentaires de base et divers ustensiles comme enjeux susceptibles de mettre tout un pays sous le joug d’un réseau qui sème perpétuellement le vent en RDC`,
+        image: "https://m.media-amazon.com/images/I/41Qp8VHvUFL.jpg",
+        price: 5,
+        lang: {
+            lingala: "Lingala",
+        }
+    },
+    {
+        id: 6,
+        title: "Sina: Das Kongo-Schicksal",
+        description: "Am 30. Juni 1960 wurde Belgisch-Kongo unabhängig. Das Land wurde umbenannt. Demokratische Republik Kongo. In Leopoldville, der heutigen Hauptstadt Kinshasa, jubelte die Menge und sang mit Begeisterung die neue Nationalhymne: Auf! Kongolesen. Verbunden durch das Schicksal. Verbunden in Anstrengung für die Selbständigkeit. Erheben wir unsere Köpfe. Seit langem verachtet. Für immer nehmen wir den schönsten Aufschwung in Frieden. … im Fleiß werden wir ein Land bauen; schöner als früher. Bürgerinnen und Bürger! Stimmen wir in würde die heiligen Hymne unserer Solidarität an … Aber was die Kongolesen von der Selbständigkeit gehofft und erwartet hatten, ist keineswegs eingetroffen. Im Gegenteil, nach dem strahlenden Morgen der Unabhängigkeitsfeiern breitete sich rasch, wie nach einem schweren Unwetter, eine Stimmung der allgemeinen Lähmung über das Land aus. Warum konnten die Kongolesen ihre Träume nicht verwirklichen? Reicht die politische Selbständigkeit doch nicht aus, um eine gemeinsame Zukunft zu bauen? Gibt es so etwas wie das kongolesische Schicksal? Die Antwort Bulayumis klingt einfach: Die Wurzel eines Menschen ist das Fundament seiner Existenz. Identitätsfindung ist der Schlüssel seiner fiktiven Aussagen, die sich teils auf Realität stützen. Das Buch soll in diesem Sinn als ein Ethno-Roman betrachtet werden.",
+        image: "https://m.media-amazon.com/images/I/41BeoEgPYsL.jpg",
+        price: 11.50,
+        lang: {
+            allemand: "Allemand",
+        }
+    },
+    {
+        id: 7,
+        title: "Das Beichten eines Afro-Wieners",
+        description: "Als austherapierter Patient lässt Pierre Ntima, ein Afro-Wiener, sein Leben und jenes seines Freundeskreises in Wien Revue passieren. Er stirbt, unmittelbar, nachdem er in Anwesenheit seiner Freunde über seine Verfehlungen und die Nichterfüllung seines Missionsauftrags in Europa gebeichtet hat. (...) En Passant erfährt der Leser neben den wenig bekannten Odysseen afrikanischer Flüchtlinge und Studentinnen und Studenten - die durch den Zusammenbruch des Ostblocks noch besondere Verwicklung erfuhren - einiges über gesellschaftliche Eigenarten Afrika, aber auch in Europa und insbesondere in Österreich.",
+        image: "https://m.media-amazon.com/images/I/51nsKUwku3L.jpg",
+        price: 5,
+        lang: {
+            allemand: "Allemand",
+        }
+    },
+    {
+        id: 8,
+        title: "Dealer wider Willen ? Wege afrikanischer Migrantinnen und Migranten nach / in Österreich",
+        description: `Die in Österreich ansässigen Afrikanerinnen sind gegen das Afro-Dealertum und die pauschale Verurteilung: Afrikaner sind Drogendealer!" Dennoch: Es gibt in Österreich Drogendealer afrikanischer Herkunft. Was soll man tun Softcover Kein Schutzumschlag 2005 Geringe Gebrauchsspuren Oiginalausgabe.`,
+
+        image: "https://m.media-amazon.com/images/I/51qNMrS+VJL.jpg",
+        price: 5,
+        lang: {
+            allemand: "Allemand",
+        }
+    },
+    {
+        id: 9,
+        title: "Die Fledermaus und die Sonne / The Bat and the Sun: Eine Symphonische Dichtung / A Symphonic Poem",
+        description: `Die Fledermausmutter liegt im Sterben und beauftragt ihr Kind, ihr Begräbnis vorzubereiten. Freunde und Verwandte müssen eingeladen werden, vor allem soll aber der letzte Wunsch der Mutter erfüllt werden: während der Trauerzeremonie soll die Sonne stillstehen. Doch so etwas ist der Sonne unmöglich. Die Fledermaus wendet sich enttäuscht von ihr ab und fliegt ab jetzt nur mehr bei Nacht.
+            The Mother Bat is near to death. She charges her child with the preparation of her own funeral. Friends and relatives must be invited but above all the last wish of the mother needs to be fulfilled: the sun should stand still during the funeral ceremony. Unfortunately that is impossible for the sun. Deeply disappointed the bat turns away from the sun and flies from then on only by night.`,
+
+        image: "https://m.media-amazon.com/images/I/51x0GBVaDbL.jpg",
+        price: 7.66,
+        lang: {
+            allemand: "Allemand",
+            anglais: "Anglais",
         }
     },
 ]                                                                                                 
